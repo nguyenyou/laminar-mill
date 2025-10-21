86f7d53d9fff8571ce4114a60d8fa576d7d6df31
nguyenyou <tunt081295@gmail.com>
2025-10-21 09:04:19 +0700

    🚢 ship it

--- airstream/src/io/github/nguyenyou/airstream/ownership/OneTimeOwner.scala
+++ airstream/src/io/github/nguyenyou/airstream/ownership/OneTimeOwner.scala
@@ -3,14 +3,13 @@ package io.github.nguyenyou.airstream.ownership
 /** Owner that can not be used after it was killed. Used in Laminar via [[DynamicOwner]].
   *
   * @param onAccessAfterKilled
-  *          Called if you attempt to use this owner after it was killed.
-  *          It's intended to log and/or throw for reporting / debugging purposes.
+  *   Called if you attempt to use this owner after it was killed. It's intended to log and/or throw for reporting / debugging purposes.
   */
 class OneTimeOwner(onAccessAfterKilled: () => Unit) extends Owner {
 
   private var _isKilledForever: Boolean = false
 
-  @inline def isKilledForever: Boolean = _isKilledForever
+  inline def isKilledForever: Boolean = _isKilledForever
 
   override private[ownership] def own(subscription: Subscription): Unit = {
     if (_isKilledForever) {
