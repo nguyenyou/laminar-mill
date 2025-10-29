552b59b5bc888766a74d4e4341c89e4478ab345b
nguyenyou <tunt081295@gmail.com>
2025-10-29 07:23:52 +0700

    🚢 ship it

--- www/src/www/floating/Flip.scala
+++ www/src/www/floating/Flip.scala
@@ -8,6 +8,9 @@ import io.github.nguyenyou.floatingUI.middleware.ShiftMiddleware
 import org.scalajs.dom
 
 def Flip() = {
+  val scrollRef = Var[Option[HtmlElement]](None)
+  val referenceRef = Var[Option[HtmlElement]](None)
+  val floatingRef = Var[Option[HtmlElement]](None)
 
   div(
     h1("Flip"),
@@ -15,16 +18,19 @@ def Flip() = {
     div(
       className := "container",
       div(
+        onMountCallback { ctx => scrollRef.set(Some(ctx.thisNode)) },
         className := "scroll",
         dataAttr("x") := "",
         position := "relative",
         // Reference element
         div(
+          onMountCallback { ctx => referenceRef.set(Some(ctx.thisNode)) },
           className := "reference",
           "Reference"
         ),
         // Floating element
         div(
+          onMountCallback { ctx => floatingRef.set(Some(ctx.thisNode)) },
           className := "floating",
           "Floating"
         )
