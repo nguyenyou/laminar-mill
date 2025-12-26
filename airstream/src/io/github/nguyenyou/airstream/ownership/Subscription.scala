5ff077939aaf844d556bcd7d22b484baa050de78
nguyenyou <tunt081295@gmail.com>
2025-12-26 19:43:57 +0700

    no brace

--- airstream/src/io/github/nguyenyou/airstream/ownership/Subscription.scala
+++ airstream/src/io/github/nguyenyou/airstream/ownership/Subscription.scala
@@ -3,20 +3,19 @@ package io.github.nguyenyou.airstream.ownership
 // @TODO[API] Change cleanup from () => Unit to : => Unit? Would it be possible to override such a field?
 /** Represents a leaky resource that needs to be cleaned up.
   *
-  * Subscription is linked for its life to a given owner.
-  * It can be killed by that owner, or externally using .kill().
+  * Subscription is linked for its life to a given owner. It can be killed by that owner, or externally using .kill().
   *
   * See also: Ownership documentation, as well as [[Owner]] scaladoc
   *
-  * @param cleanup Note: Must not throw!
+  * @param cleanup
+  *   Note: Must not throw!
   */
 class Subscription(
   private[ownership] val owner: Owner,
   cleanup: () => Unit
-) {
-
-  /** Make sure we only kill any given Subscription once. Just a sanity check against bad user logic,
-    * e.g. calling .kill() manually when `owner` has already killed this subscription.
+):
+  /** Make sure we only kill any given Subscription once. Just a sanity check against bad user logic, e.g. calling .kill() manually when
+    * `owner` has already killed this subscription.
     */
   final private var _isKilled = false
 
@@ -24,22 +23,16 @@ class Subscription(
 
   def isKilled: Boolean = _isKilled
 
-  def kill(): Unit = {
+  def kill(): Unit =
     safeCleanup()
     owner.onKilledExternally(this)
-  }
 
   // @TODO[API] This method exists only due to permissions. Is there another way?
-  @inline private[ownership] def onKilledByOwner(): Unit = {
+  @inline private[ownership] def onKilledByOwner(): Unit =
     safeCleanup()
-  }
 
-  private def safeCleanup(): Unit = {
-    if (!_isKilled) {
+  private def safeCleanup(): Unit =
+    if (!_isKilled) then
       cleanup()
       _isKilled = true
-    } else {
-      throw new Exception("Can not kill Subscription: it was already killed.")
-    }
-  }
-}
+    else throw new Exception("Can not kill Subscription: it was already killed.")
