ffaff1fee4a01cb84b8dfe94f30039fe6ab84dab
nguyenyou <tunt081295@gmail.com>
2025-12-26 19:55:16 +0700

    no brace

--- airstream/src/io/github/nguyenyou/airstream/ownership/ManualOwner.scala
+++ airstream/src/io/github/nguyenyou/airstream/ownership/ManualOwner.scala
@@ -1,14 +1,11 @@
 package io.github.nguyenyou.airstream.ownership
 
-/** All this class does is expose the method to kill subscriptions.
-  * Just a small convenience for ad-hoc integrations
+/** All this class does is expose the method to kill subscriptions. Just a small convenience for ad-hoc integrations
   *
-  * Please note that this is NOT a [[OneTimeOwner]].
-  * If that's what you need, copy this code but extend [[OneTimeOwner]] instead of [[Owner]].
+  * Please note that this is NOT a [[OneTimeOwner]]. If that's what you need, copy this code but extend [[OneTimeOwner]] instead of
+  * [[Owner]].
   */
-class ManualOwner extends Owner {
+class ManualOwner extends Owner:
 
-  override def killSubscriptions(): Unit = {
+  override def killSubscriptions(): Unit =
     super.killSubscriptions()
-  }
-}
